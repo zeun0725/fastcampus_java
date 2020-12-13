a964c3b62687f417814a5768af21fb6b0ca8f6fc
parkjieun <wldmsdl3187@naver.com>
2020-12-14 02:10:14 +0900

    lombok

--- src/main/java/kr/co/fastcampus/Main.java
+++ src/main/java/kr/co/fastcampus/Main.java
@@ -8,50 +8,32 @@ import java.sql.*;
 class Main{
 	private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws ClassNotFoundException {
 		logger.info("hello world!");
-		Connection connection = null;
-		Statement statement = null;
-		try {
-			Class.forName("org.h2.Driver");
-			String url = "jdbc:h2:mem:test;MODE=MySQL;";
-			connection = DriverManager.getConnection(url,"sa","");
-			statement = connection.createStatement();
 
+		Class.forName("org.h2.Driver");
+		var url = "jdbc:h2:mem:test;MODE=MySQL;"; //var => string 타입 유추
+
+		try(var connection = DriverManager.getConnection(url, "sa", "");
+			var statement = connection.createStatement()) {
 			connection.setAutoCommit(false);
 
 			statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-			statement.executeUpdate("insert into member(username, password) values('parkjieun', '1234')");
 
-
-
-			ResultSet resultSet = statement.executeQuery("select id, username, password from member");
-			while(resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String username = resultSet.getString("username");
-				String password = resultSet.getString("password");
-
-				logger.info("id:"+id+", username:"+username+", password:"+password);
-			}
-
-			connection.commit();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
 			try {
-				statement.close();
-			} catch (SQLException throwables) {
-				throwables.printStackTrace();
-			}
-			try {
+				statement.executeUpdate("insert into member(username, password) values('parkjieun', '1234')");
+				connection.commit();
+			} catch (SQLException e){
 				connection.rollback();
-				connection.close();
-			} catch (SQLException throwables) {
-				throwables.printStackTrace();
 			}
+
+			var resultSet = statement.executeQuery("select id, username, password from member");
+			while(resultSet.next()) {
+				var member = new Member(resultSet);
+				logger.info(member.toString());
+			}
+		} catch (SQLException e) {
+
 		}
 
 	}
---
+++ src/main/java/kr/co/fastcampus/Member.java
@@ -0,0 +1,50 @@
+package kr.co.fastcampus;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Member {
+    private int id;
+    private String username;
+    private String password;
+
+    public Member(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getInt("id");
+            this.username = resultSet.getString("username");
+            this.password = resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "id:"+id+", username:"+username+", password:"+password;
+    }
+}
