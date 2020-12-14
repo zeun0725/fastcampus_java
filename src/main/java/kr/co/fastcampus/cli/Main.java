e30d2b55b3ab686cb0fa05775a47bb0218bea9dc
parkjieun <wldmsdl3187@naver.com>
2020-12-15 01:23:32 +0900

    servlet

---
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -0,0 +1,41 @@
+package kr.co.fastcampus.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+class Main{
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		logger.info("hello world!");
+
+		Class.forName("org.h2.Driver");
+		var url = "jdbc:h2:mem:test;MODE=MySQL;"; //var => string 타입 유추
+
+		try(var connection = DriverManager.getConnection(url, "sa", "");
+			var statement = connection.createStatement()) {
+			connection.setAutoCommit(false);
+
+			statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
+
+			try {
+				statement.executeUpdate("insert into member(username, password) values('parkjieun', '1234')");
+				connection.commit();
+			} catch (SQLException e){
+				connection.rollback();
+			}
+
+			var resultSet = statement.executeQuery("select id, username, password from member");
+			while(resultSet.next()) {
+				var member = new Member(resultSet);
+
+				logger.info(member.toString());
+			}
+		} catch (SQLException e) {
+
+		}
+
+	}
+}
---
+++ src/main/java/kr/co/fastcampus/cli/Member.java
@@ -0,0 +1,33 @@
+package kr.co.fastcampus.cli;
+
+import lombok.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Member {
+    private int id;
+    @NonNull private String username;
+    @NonNull private String password;
+
+
+    public Member(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getInt("id");
+            this.username = resultSet.getString("username");
+            this.password = resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
