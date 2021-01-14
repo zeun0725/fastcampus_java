aa079f8911b8f48b8549b8b9a714e7cb39620c55
parkjieun <wldmsdl3187@naver.com>
2021-01-15 00:41:46 +0900

    bean scopes 관련 브랜치가 제대로 정리가 안된 문제가 있음, main, bean, dependencies 모두를 봐야 함

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -6,28 +6,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 import java.sql.SQLException;
+
 @Slf4j
 class Main{
 	private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		log.info("hello world!");
-		try {
-			Class.forName("org.h2.Driver");
-		} catch (ClassNotFoundException e){
-			e.printStackTrace();
-		}
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml", "bean.xml");
-		A a1 = context.getBean("A", A.class);
-		A a2 = context.getBean("A", A.class);
-		log.info("result: " + (a1 == a2));  //true  -> spring 은 싱글톤이 디폴트 // false -> scope="prototype" 넣어주기
 
-//		Dao dao = context.getBean("dao",Dao.class);
-//
-//		dao.run();
+		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+		Dao2 dao2 = context.getBean("dao2",Dao2.class);
+		dao2.run();
 
 
 
