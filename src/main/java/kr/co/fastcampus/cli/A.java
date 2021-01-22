d604945b9d0876e50b97d1e8507124d3ffb0e56b
parkjieun <wldmsdl3187@naver.com>
2021-01-23 01:50:38 +0900

    Resource annotation and  Value annotation

--- src/main/java/kr/co/fastcampus/cli/A.java
+++ src/main/java/kr/co/fastcampus/cli/A.java
@@ -2,16 +2,18 @@ package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 @Slf4j
 public class A {
-    @Autowired @Qualifier("b1") private B b;
-
+    @Resource(name="appBeanB1") private B b;
     @Autowired private ApplicationContext context;
+    @Value("${catalog.name}") String catalogName;
+    @Value("#{systemProperties['java.home']}") String property;
 
     @PostConstruct
     void init(){
