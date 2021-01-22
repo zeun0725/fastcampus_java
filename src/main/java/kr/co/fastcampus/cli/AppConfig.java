23a02764d4ea855864e089703642cce4c0f5d058
parkjieun <wldmsdl3187@naver.com>
2021-01-23 01:39:15 +0900

    Qualifier annotation

--- src/main/java/kr/co/fastcampus/cli/AppConfig.java
+++ src/main/java/kr/co/fastcampus/cli/AppConfig.java
@@ -1,5 +1,6 @@
 package kr.co.fastcampus.cli;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
     @Bean
+    @Qualifier("b1")
     public B b1(){
+        return new B();
+    }
+
+    @Bean
+    @Qualifier("b2")
+    public B b2(){
         return new B();
     }
 
