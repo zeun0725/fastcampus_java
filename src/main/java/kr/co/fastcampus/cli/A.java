1f0c8299ef947faa39936c7a0373089fe83c4998
parkjieun <wldmsdl3187@naver.com>
2021-01-31 10:59:42 +0900

    JSR 330 & 많이 사용하지 않지만 이런 것들이 있다는 것 정도만 알아두기

--- src/main/java/kr/co/fastcampus/cli/A.java
+++ src/main/java/kr/co/fastcampus/cli/A.java
@@ -4,15 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 @Slf4j
-@Component
+@Named("a")
 public class A {
-//    @Autowired private B b;
+    @Inject
+    private B b;
     @Autowired private ApplicationContext context;
     @Value("${catalog.name}") String catalogName;
     @Value("#{systemProperties['java.home']}") String property;
